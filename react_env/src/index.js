65b2e8236fe8ddbff196403f0387f39a27c178e0
sadiq <mail@sadiq-pasha.com>
2023-01-30 13:00:29 -0800

    part 2 exercises 12-15

--- react_env/src/index.js
+++ react_env/src/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import App from './App';
 import UnicafeApp from './1.6-1.11_UnicafeApp';
 import AnecdotesApp from './1.12-1.14_Anecdotes';
 import CoursesApp from './1.1-1.5_Courses';
 import CourseInformation from './2.1-2.5_CourseInformation';
+import PhoneBook from './2.6-2.11_Phonebook';
+
+// const notes = [
+//     {
+//       id: 1,
+//       content: 'HTML is easy',
+//       date: '2019-05-30T17:30:31.098Z',
+//       important: true
+//     },
+//     {
+//       id: 2,
+//       content: 'Browser can execute only JavaScript',
+//       date: '2019-05-30T18:39:34.091Z',
+//       important: false
+//     },
+//     {
+//       id: 3,
+//       content: 'GET and POST are the most important methods of HTTP protocol',
+//       date: '2019-05-30T19:20:14.298Z',
+//       important: true
+//     }
+//   ]
+
 
 ReactDOM.createRoot(document.getElementById('courses')).render(<CoursesApp />);
 ReactDOM.createRoot(document.getElementById('unicafe')).render(<UnicafeApp />);
 ReactDOM.createRoot(document.getElementById('anecdotes')).render(<AnecdotesApp />);
 ReactDOM.createRoot(document.getElementById('courseInformation')).render(<CourseInformation/>)
+ReactDOM.createRoot(document.getElementById('forms')).render(<PhoneBook/>)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
